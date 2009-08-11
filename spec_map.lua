720d3f4992b65a7dc2d6fcb3af9f758aaf4286a1
Steve Yen <steve.yen@gmail.com>
2009-08-11 11:03:28 -0700

    shorter spec_map, more err handling

--- spec_map.lua
+++ spec_map.lua
@@ -4,12 +4,12 @@ spec_map = {
       for key in itr do
         data = map_data[key]
         if data then
-          asock.send(sess_addr, skt,
-                     "VALUE " .. key .. "\r\n" .. data)
+          if not sock_send(skt, "VALUE " .. key .. "\r\n" .. data) then
+            return false
+          end
         end
       end
-      asock.send(sess_addr, skt, "END\r\n")
-      return true
+      return sock_send(skt, "END\r\n") ~= nil
     end,
 
   set =
@@ -21,17 +21,16 @@ spec_map = {
       if key and flgs and expt and size then
         size = tonumber(size)
         if size >= 0 then
-          local data = asock.recv(sess_addr, skt,
-                                  tonumber(size) + 2)
+          local data = sock_recv(skt, tonumber(size) + 2)
           if data then
             map_data[key] = data
-            asock.send(sess_addr, skt, "STORED\r\n")
-            return true
+            return sock_send(skt, "STORED\r\n") ~= nil
+          else
+            return false
           end
         end
       end
-      asock.send(sess_addr, skt, "ERROR\r\n")
-      return true
+      return sock_send(skt, "ERROR\r\n") ~= nil
     end,
 
   delete =
@@ -40,21 +39,18 @@ spec_map = {
       if key then
         if map_data[key] then
           map_data[key] = nil
-          asock.send(sess_addr, skt, "DELETED\r\n")
+          return sock_send(skt, "DELETED\r\n") ~= nil
         else
-          asock.send(sess_addr, skt, "NOT_FOUND\r\n")
+          return sock_send(skt, "NOT_FOUND\r\n") ~= nil
         end
-      else
-        asock.send(sess_addr, skt, "ERROR\r\n")
       end
-      return true
+      return sock_send(skt, "ERROR\r\n") ~= nil
     end,
 
   flush_all =
     function(map_data, sess_addr, skt, itr)
       map_data = {}
-      asock.send(sess_addr, skt, "OK\r\n")
-      return true
+      return sock_send(skt, "OK\r\n") ~= nil
     end,
 
   quit =
