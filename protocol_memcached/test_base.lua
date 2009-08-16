0512a0d86eab1859a9870d30ce5f71edef2d5833
Steve Yen <steve.yen@gmail.com>
2009-08-16 08:47:34 -0700

    printa does recursive dump

--- protocol_memcached/test_base.lua
+++ protocol_memcached/test_base.lua
@@ -6,9 +6,22 @@ require 'util'
 
 p = print
 
-function printa(a)
-  for i, v in ipairs(a) do
-    p(i, v)
+function printa(a, prefix)
+  prefix = prefix or ""
+  if type(a) == 'table' then
+    for i, v in pairs(a) do
+      if type(v) == 'table' then
+        p(prefix .. i .. ' {')
+        printa(v, prefix .. '  ')
+        p(prefix .. '}')
+      else
+        p(prefix .. i, v)
+      end
+    end
+  elseif a == nil then
+    p(prefix .. "[nil]")
+  else
+    p(prefix .. a)
   end
 end
 
@@ -18,6 +31,7 @@ pa = printa
 
 got_list = {}
 function fresh()
+  p('-------------------------')
   got_list = {}
 end
 
@@ -26,24 +40,46 @@ function got(...)
   pa(arg)
 end
 
-function expected(...)
-  assert(#got_list == #arg, "#got_list != #arg, " .. #got_list .. " " .. #arg)
-  for i = 1, #arg do
-    local expect = arg[i]
-    if type(expect) == "string" then
-      expect = {expect}
+------------------------------------------
+
+function tree_match(expect, actual)
+  assert(type(expect) == type(actual),
+         "type mismatch, " .. type(expect) .. " " .. type(actual))
+
+  if type(expect) == 'string' then
+    if not string.find(actual, "^" .. expect) then
+      p("expected", expect, "actual", actual)
+      assert(false)
     end
-    assert(#(got_list[i]) == #expect)
-    for j = 1, #expect do
-      if expect[j] == nil then
-        assert(got_list[i][j] == nil)
-      else
-        if not string.find(got_list[i][j], "^" .. expect[j]) then
-          p("expected", expect[j], "got", got_list[i][j])
-          assert(false)
-        end
-      end
-    end
+    return
   end
+
+  if type(expect) ~= 'table' then
+    assert(expect == actual, "unequal " .. expect .. " " .. actual)
+    return
+  end
+
+  assert(#expect == #actual,
+         "table size mismatch, " .. #expect .. " " .. #actual)
+
+  for i = 1, #expect do
+    tree_match(expect[i], actual[i])
+  end
+end
+
+function expected(...)
+  local expect = arg
+  if #expect == 1 and type(expect[1]) == 'string' then
+    expect = {expect}
+  end
+
+  p("--------------")
+  p("expect")
+  pa(expect)
+
+  p("actual")
+  pa(got_list)
+
+  tree_match(expect, got_list)
 end
 
