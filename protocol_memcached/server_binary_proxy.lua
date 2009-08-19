05af304871078809e8e5ee73cd90800ff4c55c01
Steve Yen <steve.yen@gmail.com>
2009-08-19 11:19:37 -0700

    forward_simple/broadcast helper funcs

--- protocol_memcached/server_binary_proxy.lua
+++ protocol_memcached/server_binary_proxy.lua
@@ -11,7 +11,9 @@ local SUCCESS = mpb.response_stats.SUCCESS
 
 ------------------------------------------------------
 
-local function simple_forward(pool, skt, req, args)
+-- For binary commands that just do a simple command forward.
+--
+local function forward_simple(pool, skt, req, args)
   args.req = req
 
   local downstream_addr = pool.choose(args.key)
@@ -25,71 +27,78 @@ local function simple_forward(pool, skt, req, args)
   return false -- TODO: send err response instead?
 end
 
+-- For binary commands that just do a broadcast scatter/gather.
+--
+local function forward_broadcast(pool, skt, req, args, skt_callback)
+  local opcode = pack.opcode(req, 'request')
+
+  args.req = req
+
+  local n = 0
+  pool.each(
+    function(downstream_addr)
+      apo.send(downstream_addr, apo.self_address(),
+               false, opcode, args, skt_callback)
+      n = n + 1
+    end)
+
+  local oks = 0 -- TODO: Do something with oks count.
+  for i = 1, n do
+    if apo.recv() then
+      oks = oks + 1
+    end
+  end
+
+  local res =
+    pack.create_response(opcode, {
+      status = SUCCESS,
+      opaque = pack.opaque(req, 'request')
+    })
+  return sock_send(skt, res)
+end
+
 ------------------------------------------------------
 
-msbp[mpb.command.GET] =
-  function(pool, skt, req, args)
-  end
+local c = mpb.command
+local a = {
+  c.GET,
+  c.SET,
+  c.ADD,
+  c.REPLACE,
+  c.DELETE,
+  c.INCREMENT,
+  c.DECREMENT,
+  c.GETQ,
+  c.GETK,
+  c.GETKQ,
+  c.APPEND,
+  c.PREPEND,
+  c.STAT,
+  c.SETQ,
+  c.ADDQ,
+  c.REPLACEQ,
+  c.DELETEQ,
+  c.INCREMENTQ,
+  c.DECREMENTQ,
+  c.FLUSHQ,
+  c.APPENDQ,
+  c.PREPENDQ
+}
 
-msbp[mpb.command.SET] = simple_forward
+for i = 1, #a do
+  msbp[a[i]] = forward_simple
+end
 
-msbp[mpb.command.ADD] =
-  function(pool, skt, req, args)
-  end
-
-msbp[mpb.command.REPLACE] =
-  function(pool, skt, req, args)
-  end
-
-msbp[mpb.command.DELETE] = simple_forward
-
-msbp[mpb.command.INCREMENT] =
-  function(pool, skt, req, args)
-  end
-
-msbp[mpb.command.DECREMENT] =
-  function(pool, skt, req, args)
-  end
-
-msbp[mpb.command.QUIT] =
-  function(pool, skt, req, args)
-    return false
-  end
+------------------------------------------------------
 
 msbp[mpb.command.FLUSH] =
   function(pool, skt, req, args)
-    args.req = req
-
-    local n = 0
-    pool.each(
-      function(downstream_addr)
-        apo.send(downstream_addr, apo.self_address(),
-                 false, mpb.command.FLUSH, args)
-        n = n + 1
-      end)
-
-    local oks = 0
-    for i = 1, n do
-      if apo.recv() then
-        oks = oks + 1
-      end
-    end
-
-    local res =
-      pack.create_response(mpb.command.FLUSH, {
-        status = SUCCESS,
-        opaque = pack.opaque(req, 'request')
-      })
-    return sock_send(skt, res)
-  end
-
-msbp[mpb.command.GETQ] =
-  function(pool, skt, req, args)
+    return forward_broadcast(pool, skt, req, args, nil)
   end
 
 msbp[mpb.command.NOOP] =
   function(pool, skt, req, args)
-    local function skt_send(head, body)
+    local function skt_callback(head, body)
       if pack.opcode(head, 'response') == mpb.command.NOOP then
         return
       end
@@ -102,91 +111,25 @@ msbp[mpb.command.NOOP] =
       return sock_send(skt, msg)
     end
 
-    args.req = req
-
-    local n = 0
-    pool.each(
-      function(downstream_addr)
-        apo.send(downstream_addr, apo.self_address(),
-                 false, mpb.command.NOOP, args,
-                 skt_send)
-        n = n + 1
-      end)
-
-    local oks = 0
-    for i = 1, n do
-      if apo.recv() then
-        oks = oks + 1
-      end
-    end
-
-    local res =
-      pack.create_response(mpb.command.NOOP, {
-        status = SUCCESS,
-        opaque = pack.opaque(req, 'request')
-      })
-    return sock_send(skt, res)
+    return forward_broadcast(pool, skt, req, args, skt_callback)
   end
+
+------------------------------------------------------
+
+msbp[mpb.command.QUIT] =
+  function(pool, skt, req, args)
+    return false
+  end
+
+msbp[mpb.command.QUITQ] = msbp[mpb.command.QUIT]
+
+------------------------------------------------------
 
 msbp[mpb.command.VERSION] =
   function(pool, skt, req, args)
   end
 
-msbp[mpb.command.GETK] =
-  function(pool, skt, req, args)
-  end
-
-msbp[mpb.command.GETKQ] = simple_forward
-
-msbp[mpb.command.APPEND] =
-  function(pool, skt, req, args)
-  end
-
-msbp[mpb.command.PREPEND] =
-  function(pool, skt, req, args)
-  end
-
 msbp[mpb.command.STAT] =
-  function(pool, skt, req, args)
-  end
-
-msbp[mpb.command.SETQ] =
-  function(pool, skt, req, args)
-  end
-
-msbp[mpb.command.ADDQ] =
-  function(pool, skt, req, args)
-  end
-
-msbp[mpb.command.REPLACEQ] =
-  function(pool, skt, req, args)
-  end
-
-msbp[mpb.command.DELETEQ] =
-  function(pool, skt, req, args)
-  end
-
-msbp[mpb.command.INCREMENTQ] =
-  function(pool, skt, req, args)
-  end
-
-msbp[mpb.command.DECREMENTQ] =
-  function(pool, skt, req, args)
-  end
-
-msbp[mpb.command.QUITQ] =
-  function(pool, skt, req, args)
-  end
-
-msbp[mpb.command.FLUSHQ] =
-  function(pool, skt, req, args)
-  end
-
-msbp[mpb.command.APPENDQ] =
-  function(pool, skt, req, args)
-  end
-
-msbp[mpb.command.PREPENDQ] =
   function(pool, skt, req, args)
   end
 
