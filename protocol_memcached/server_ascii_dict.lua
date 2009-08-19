a016f1c8d50350f9da55697a9f3907b85e4f9455
Steve Yen <steve.yen@gmail.com>
2009-08-19 14:00:52 -0700

    set/add/replace for ascii

--- protocol_memcached/server_ascii_dict.lua
+++ protocol_memcached/server_ascii_dict.lua
@@ -1,3 +1,64 @@
+local dict_update_map = {
+  set =
+    function(dict, key, flag, expire, data)
+      dict.tbl[key] = data
+      return true, "STORED"
+    end,
+  add =
+    function(dict, key, flag, expire, data)
+      if dict.tbl[key] then
+        return false, "NOT_STORED"
+      end
+      dict.tbl[key] = data
+      return true, "STORED"
+    end,
+  replace =
+    function(dict, key, flag, expire, data)
+      if not dict.tbl[key] then
+        return false, "NOT_STORED"
+      end
+      dict.tbl[key] = data
+      return true, "STORED"
+    end,
+  append =
+    function(dict, key, flag, expire, data)
+      dict.tbl[key] = (dict.tbl[key] or "") .. data
+      return true, "STORED"
+    end,
+  prepend =
+    function(dict, key, flag, expire, data)
+      dict.tbl[key] = data .. (dict.tbl[key] or "")
+      return true, "STORED"
+    end
+}
+
+local function dict_update(dict, skt, cmd, arr)
+  local key    = arr[1]
+  local flag   = arr[2]
+  local expire = arr[3]
+  local size   = arr[4]
+
+  if key and flag and expire and size then
+    size = tonumber(size)
+    if size >= 0 then
+      local data, err = sock_recv(skt, tonumber(size) + 2)
+      if not data then
+        return data, err
+      end
+
+      local ok, msg =
+        dict_update_map[cmd](dict, key, flag, expire,
+                             string.sub(data, 1, -3))
+
+      return sock_send(skt, msg .. "\r\n")
+    end
+  end
+
+  return sock_send(skt, "ERROR\r\n")
+end
+
+---------------------------------------------------
+
 memcached_server_ascii_dict = {
   get =
     function(dict, skt, cmd, arr)
@@ -17,29 +78,11 @@ memcached_server_ascii_dict = {
       return sock_send(skt, "END\r\n")
     end,
 
-  set =
-    function(dict, skt, cmd, arr)
-      local key    = arr[1]
-      local flag   = arr[2]
-      local expire = arr[3]
-      local size   = arr[4]
-
-      if key and flag and expire and size then
-        size = tonumber(size)
-        if size >= 0 then
-          local data, err = sock_recv(skt, tonumber(size) + 2)
-          if not data then
-            return data, err
-          end
-
-          dict.tbl[key] = string.sub(data, 1, -3)
-
-          return sock_send(skt, "STORED\r\n")
-        end
-      end
-
-      return sock_send(skt, "ERROR\r\n")
-    end,
+  set     = dict_update,
+  add     = dict_update,
+  replace = dict_update,
+  append  = dict_update,
+  prepend = dict_update,
 
   delete =
     function(dict, skt, cmd, arr)
